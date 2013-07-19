db863ab3e56eb7bafc5746b618a1772da0ad2c31
nwagensonner <n.wagensonner@voycer.com>
2013-07-19 11:38:16 +0200

    fixed another bug

--- create-notes.sh
+++ create-notes.sh
@@ -1,3 +1,3 @@
 #/!bin/bash
-(echo "{toc}"; cd $1 git tag | grep "^[0-9]\{1,\}\(\.[0-9]\{1,\}\)*$" | sort -t. -k 1,1rn -k 2,2rn -k 3,3rn -k 4,4rn | xargs git show --name-only --pretty="format:%h - %an, %ar : %s" ; ) |  sed 's/tag /h2. tag /g' | php app/console confluence:release-notes --username="$2" --password="$3" "https://confluence.voycer.com/rpc/xmlrpc" "$4"
+(echo "{toc}"; cd $1; git tag | grep "^[0-9]\{1,\}\(\.[0-9]\{1,\}\)*$" | sort -t. -k 1,1rn -k 2,2rn -k 3,3rn -k 4,4rn | xargs git show --name-only --pretty="format:%h - %an, %ar : %s" ; ) |  sed 's/tag /h2. tag /g' | php app/console confluence:release-notes --username="$2" --password="$3" "https://confluence.voycer.com/rpc/xmlrpc" "$4"
 
